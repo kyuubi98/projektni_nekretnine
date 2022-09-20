d484642dab36b862488c5e83bfbf2ce11497c5a9
marinagavrilovic <marinagavrilovic391@gmail.com>
2022-09-20 22:26:10 +0200

    vracanje tabele

---
+++ pro.php
@@ -0,0 +1,25 @@
+<?php
+
+
+include 'init.php';
+
+
+$sql = "SELECT * FROM nekretnina";
+$result = $mysqli->query($sql);
+
+
+while($row = $result->fetch_array(MYSQLI_ASSOC)){
+  $data[] = $row;
+}
+
+
+$results = ["sEcho" => 1,
+        	"iTotalRecords" => count($data),
+        	"iTotalDisplayRecords" => count($data),
+        	"aaData" => $data ];
+
+
+echo json_encode($results);
+
+ 
+?>
