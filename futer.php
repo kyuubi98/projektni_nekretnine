5e795b82192120243ea291c0154f15ed5075bf23
Tamara Vucetic <tamaravucetic98@gmail.com>
2022-09-21 12:21:20 +0200

    mijenjanje footera

--- futer.php
+++ futer.php
@@ -1,5 +1,5 @@
 <footer>
     <div class="col-lg-12 top2 bottom2">
-      <div class="text-center">Nekretnine &copy; 2021. Copyright Marina Gavrilovic</i></a></div>
+      <div class="text-center">Nekretnine &copy; 2022. Copyright Ekskluzivne nekretnine</i></a></div>
     </div>
 </footer>
